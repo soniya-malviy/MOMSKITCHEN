eb86a32803d6dda9737daccc10e8bd6edf0b209c
Soniya <soniya.04malviya@gmail.com>
2025-11-25 19:04:37 +0530

    resolved 2

--- src/config/db.js
+++ src/config/db.js
@@ -2,6 +2,9 @@ import { drizzle } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import { ENV } from "./env.js";
 import * as schema from "../db/schema.js";
+import dotenv from "dotenv";
 
-const sql = neon(ENV.DATABASE_URL);
+dotenv.config(); 
+
+const sql = neon(process.env.DATABASE_URL);
 export const db = drizzle(sql, { schema });
