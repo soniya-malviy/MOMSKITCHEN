eb2d1e7c769ab8becfd392876741643c3f9d9aa6
Soniya <soniya.04malviya@gmail.com>
2025-11-25 18:52:43 +0530

    cron.js edit

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import "dotenv/config";
 import { ENV } from "./config/env.js";
-
-
+import { db } from "./config/db.js";
+import { favoritesTable } from "./db/schema.js";
+import job from "./config/cron.js";
 const app = express();
 const PORT = ENV.PORT || 5001;
 
+if(process.env.NODE_ENV === "production") job.start();
+app.use(express.json());
 
 app.get("/api/health", (req, res) => {
     res.status(200).json({success:true});
@@ -37,6 +40,44 @@ app.post("/api/favorites", async (req, res) => {
     res.status(500).json({ error: "Something went wrong" });
   }
 });
+
+// get favorites by user id to show in favorites page
+
+app.get("/api/favorites/:userId", async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const userFavorites = await db
+      .select()
+      .from(favoritesTable)
+      .where(eq(favoritesTable.userId, userId));
+
+    res.status(200).json(userFavorites);
+  } catch (error) {
+    console.log("Error fetching the favorites", error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+
+app.delete("/api/favorites/:userId/:recipeId", async (req, res) => {
+  try {
+    const { userId, recipeId } = req.params;
+
+    await db
+      .delete(favoritesTable)
+      .where(
+        and(eq(favoritesTable.userId, userId), eq(favoritesTable.recipeId, parseInt(recipeId)))
+      );
+
+    res.status(200).json({ message: "Favorite removed successfully" });
+  } catch (error) {
+    console.log("Error removing a favorite", error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
